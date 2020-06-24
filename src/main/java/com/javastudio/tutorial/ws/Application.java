315ecb7f5cdb9a0d4fc44b6b51d398208207cac7
Hossein Mohammadi <Ho.Mohammadi@persianswitch.net>
2020-06-24 00:43:26 -0700

    Add some log to project

--- src/main/java/com/javastudio/tutorial/ws/Application.java
+++ src/main/java/com/javastudio/tutorial/ws/Application.java
@@ -4,6 +4,8 @@ import com.javastudio.tutorial.ws.publisher.ConversionPublisher;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("Application starts ...");
         new Thread(ConversionPublisher::publish).start();
+        System.out.println("Application started");
     }
 }
--- src/main/java/com/javastudio/tutorial/ws/publisher/ConversionPublisher.java
+++ src/main/java/com/javastudio/tutorial/ws/publisher/ConversionPublisher.java
@@ -6,6 +6,8 @@ import javax.xml.ws.Endpoint;
 
 public class ConversionPublisher {
     public static void publish() {
+        System.out.println("Publishing exposed apis of Conversion soap webservice");
         Endpoint.publish("http://localhost:9901/UC", new ConversionImpl());
+        System.out.println("Endpoint published at http://localhost:9901/UC");
     }
 }
